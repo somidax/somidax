f110dbf9cf9eb72366dde89e46a414e627739a62
SOMIDAX <23489369+somidax@users.noreply.github.com>
2018-04-08 09:35:59 +0100

    Update translations.json.js

--- js/translations.json.js
+++ js/translations.json.js
@@ -1,14 +1,14 @@
 /* This module was module number 573 in the old packed code. It was referenced in the old code using `require(<module name>)` by the following module names:
 * ./translations.json
 */
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.translations = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 
-module.exports = {
-    en: {
-        en: 'English',
-        cn: 'English',
-        fr: 'English',
-        es: 'English',
-        ko: 'English',
+    module.exports = {
+  en: {
+    en: 'English',
+    cn: 'English',
+    fr: 'English',
+    es: 'English',
     },
     cn: { en: '中文', cn: '中文', fr: '中文', es: '中文', ko: '中文' },
     fr: {
